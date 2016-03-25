316c3aa26a46c626cd9ed9573ee5a829b60f5697
eko_fresh <erkanguzeler@gmail.com>
2016-03-25 21:11:41 +0200

    Perspektif eklendi

--- app/src/main/java/com/multiplationtable/SelectionActivity.java
+++ app/src/main/java/com/multiplationtable/SelectionActivity.java
@@ -1,6 +1,10 @@
 package com.multiplationtable;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -8,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class SelectionActivity extends AppCompatActivity  implements  View.OnClickListener{
 
@@ -15,6 +20,15 @@ public class SelectionActivity extends AppCompatActivity  implements  View.OnCli
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_selection);
+//        RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.relativeLayout1);
+//        Drawable drawable = getResources().getDrawable(R.drawable.g1);
+//
+//        relativeLayout.setBackgroundDrawable(drawable);
+
+//        Bitmap bmImg = BitmapFactory.decodeStream();
+//        BitmapDrawable background = new BitmapDrawable(bmImg);
+//        relativeLayout.setBackgroundDrawable(background);
+
         Button buttonLearn = (Button)findViewById(R.id.buttonLearn);
         Button buttonTrain = (Button)findViewById(R.id.buttonTrain);
         buttonLearn.setOnClickListener(this);
